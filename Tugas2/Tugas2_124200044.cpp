76fc989edde70960506f05b4656380d227396378
vitoananda <vtoananda@gmail.com>
2020-10-25 14:18:53 +0700

    Tugas2

--- Tugas2/Tugas2_124200044.cpp
+++ Tugas2/Tugas2_124200044.cpp
@@ -25,6 +25,9 @@ int main ()
 	else if(c >= 12 && c <= 20){
 		c=20000;
 	}
+	else if(c>20){
+		c=30000
+	}
 	
 	cout<<"Umur Anak 2	: ";
 	cin>>d;
@@ -33,6 +36,9 @@ int main ()
 	}
 	else if(d >= 12 && d <= 20){
 		d=20000;
+	}
+	else if(d>20){
+		d=30000
 	}
 	
 	cout<<"Tagihan satu bulan adalah = ";
